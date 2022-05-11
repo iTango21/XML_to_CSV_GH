df2ef0c83a6fb0746dc8e5b34bf1670d23dc0b46
iTango21 <itango21@gmail.com>
2022-05-11 11:27:52 +0300

    initialization...

---
+++ main.py
@@ -0,0 +1,14 @@
+import xml.etree.ElementTree as ET
+import csv
+import pandas as pd
+import itertools
+
+from pathlib import Path
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
